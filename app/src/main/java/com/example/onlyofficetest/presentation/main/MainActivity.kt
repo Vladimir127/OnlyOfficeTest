940d9e018c2274c60263863e2a518932c4f88c81
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2025-01-27 21:33:56 +0300

    Добавлены фрагменты для отображения четырёх разделов приложения. Добавлен и настроен BottomNavigationView для переключения между этими разделами.

---
+++ app/src/main/java/com/example/onlyofficetest/presentation/main/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.onlyofficetest.presentation.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.onlyofficetest.R
+import com.example.onlyofficetest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+    }
+}
